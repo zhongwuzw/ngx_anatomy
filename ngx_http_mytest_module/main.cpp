3e93b114c3a361b26f4d5a3d788a3771841e3258
liupb <liupb@boloomo.com>
2015-01-23 11:29:38 +0800

    Initial Commit

---
+++ ngx_http_mytest_module/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ngx_http_mytest_module
+//
+//  Created by zhongwu on 15/1/23.
+//  Copyright (c) 2015年 boloomo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
