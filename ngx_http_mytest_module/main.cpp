f9b8246db37477779fe6dcee6c53dfc6bd10fe6e
zhongwuzw <zhongwuzw@qq.com>
2015-01-30 16:51:49 +0800

    ngx_anatomy

--- ngx_http_mytest_module/main.cpp
+++ ngx_http_mytest_module/main.cpp
@@ -6,10 +6,67 @@
 //  Copyright (c) 2015年 boloomo. All rights reserved.
 //
 
-#include <iostream>
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
 
+static char *
+ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+
+static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r);
+
+static ngx_command_t ngx_http_mytest_commands[] =
+{
+    {
+        ngx_string("mytest"),
+        NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
+        ngx_http_mytest,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        0,
+        NULL
+    },
+    ngx_null_command
+};
+
+static ngx_http_module_t  ngx_http_mytest_module_ctx =
+{
+    NULL,                              /* preconfiguration */
+    NULL,                  		/* postconfiguration */
+    
+    NULL,                              /* create main configuration */
+    NULL,                              /* init main configuration */
+    
+    NULL,                              /* create server configuration */
+    NULL,                              /* merge server configuration */
+    
+    NULL,       			/* create location configuration */
+    NULL         			/* merge location configuration */
+};
+
+ngx_module_t  ngx_http_mytest_module =
+{
+    NGX_MODULE_V1,
+    &ngx_http_mytest_module_ctx,           /* module context */
+    ngx_http_mytest_commands,              /* module directives */
+    NGX_HTTP_MODULE,                       /* module type */
+    NULL,                                  /* init master */
+    NULL,                                  /* init module */
+    NULL,                                  /* init process */
+    NULL,                                  /* init thread */
+    NULL,                                  /* exit thread */
+    NULL,                                  /* exit process */
+    NULL,                                  /* exit master */
+    NGX_MODULE_V1_PADDING
+};
+
+static char *ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+    ngx_http_core_loc_conf_t *clcf;
+    
+    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+    clcf->handler = ngx_http_mytest_handler;
+    return NGX_CONF_OK;
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
 }
