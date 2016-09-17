0b366f643739ae175ea2a7de24f5da2a9c82e961
zhongwuzw <zhongwuzw@qq.com>
2016-09-17 23:46:29 +0800

    update
    update

---
+++ DerivedData/Build/Intermediates/ngx_anatomy.build/Debug/ngx_anatomy.build/Objects-normal/x86_64/ngx_modules.d
@@ -0,0 +1,19 @@
+dependencies: \
+  /Users/zhongwu/Documents/workspace/ngx_anatomy/ngx_http_mytest_module/ngx_modules.c \
+  core/ngx_config.h ngx_http_mytest_module/ngx_auto_headers.h \
+  os/unix/ngx_darwin_config.h ngx_http_mytest_module/ngx_auto_config.h \
+  core/ngx_core.h os/unix/ngx_errno.h os/unix/ngx_atomic.h \
+  os/unix/ngx_thread.h core/ngx_rbtree.h os/unix/ngx_time.h \
+  os/unix/ngx_socket.h core/ngx_string.h os/unix/ngx_files.h \
+  os/unix/ngx_shmem.h os/unix/ngx_process.h os/unix/ngx_setaffinity.h \
+  os/unix/ngx_setproctitle.h os/unix/ngx_user.h os/unix/ngx_dlopen.h \
+  core/ngx_parse.h core/ngx_parse_time.h core/ngx_log.h \
+  os/unix/ngx_alloc.h core/ngx_palloc.h core/ngx_buf.h core/ngx_queue.h \
+  core/ngx_array.h core/ngx_list.h core/ngx_hash.h core/ngx_file.h \
+  core/ngx_crc.h core/ngx_crc32.h core/ngx_murmurhash.h core/ngx_regex.h \
+  /usr/local/include/pcre.h core/ngx_radix_tree.h core/ngx_times.h \
+  core/ngx_rwlock.h core/ngx_shmtx.h core/ngx_slab.h core/ngx_inet.h \
+  core/ngx_cycle.h core/ngx_resolver.h os/unix/ngx_process_cycle.h \
+  core/ngx_conf_file.h core/ngx_module.h core/nginx.h \
+  core/ngx_open_file_cache.h os/unix/ngx_os.h os/unix/ngx_darwin.h \
+  core/ngx_connection.h core/ngx_syslog.h core/ngx_proxy_protocol.h
